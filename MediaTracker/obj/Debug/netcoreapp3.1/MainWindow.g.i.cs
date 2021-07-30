411ee1e27c179a8efd370a3897d273d5571208e5
ShlomiFridman <shlomif99@gmail.com>
2021-07-30 20:24:12 +0300

    added the project's files

--- MediaTracker/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ MediaTracker/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace MediaTracker {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MediaTracker;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MediaTracker;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);

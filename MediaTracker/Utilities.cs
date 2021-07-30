e51c516a8b79ab56c6b7f00d56dfb7cc0997aef4
ShlomiFridman <shlomif99@gmail.com>
2021-07-30 20:23:21 +0300

    added the project's files

---
+++ MediaTracker/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace MediaTracker
+{
+    static class Utilties
+    {
+
+        public static long getSize(string path)
+        {
+            if (!Directory.Exists(path) && !File.Exists(path))
+                return 0;
+            FileInfo fileInfo = new FileInfo(path);
+            if (fileInfo.Attributes.HasFlag(FileAttributes.Directory))
+            {
+                long sum = 0;
+                foreach (string subPath in Directory.GetFiles(path))
+                    sum += getSize(subPath);
+                return sum;
+            }
+            else
+                return fileInfo.Length;
+        }
+
+    }
+}
